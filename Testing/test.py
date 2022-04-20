824c6094bbb99eab0b7af1da5b5c3ed2e63fc513
Melker Widén <melker.widen@gmail.com>
2022-04-20 16:58:04 +0200

    updates

--- Testing/test.py
+++ Testing/test.py
@@ -1,8 +1,23 @@
-def clamp(val, minv, maxv):
-    return max(min(val, maxv), minv)
+# Below function will print the
+# all prime factor of given number
+def prime_factors(num):
+    # Using the while loop, we will print the number of two's that divide n
+    while num % 2 == 0:
+        print(2, )
+        num = num / 2
 
-def inv_lerp(a, b, val) -> float:
-    temp = (val - a) / (b - a)
-    return clamp(temp, 0, 1)
+    for i in range(3, int(num ** 0.5) + 1, 2):
 
-print(inv_lerp(0, 10, -1))
+        # while i divides n , print i ad divide n
+        while num % i == 0:
+            print(i, )
+            num = num / i
+    if num > 2:
+        print(num)
+    # calling function
+
+
+# num = 200
+prime_factors(21)
+print()
+prime_factors(28)
