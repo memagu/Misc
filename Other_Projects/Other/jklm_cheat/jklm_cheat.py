3a759c9c05ebe50daeaa68ef9741a29a6d40ac43
Melker Widén <melker.widen@gmail.com>
2022-05-05 17:42:01 +0200

    updates

--- Other_Projects/Other/jklm_cheat/jklm_cheat.py
+++ Other_Projects/Other/jklm_cheat/jklm_cheat.py
@@ -1,19 +1,31 @@
-words = []
-used = []
+def setup() -> None:
+    ver = 1.0
+    prog_name = __file__.split('\\')[-1]
 
-with open("words_alpha.txt", "r") as f:
-    lines = f.readlines()
-    for line in lines:
-        words.append(line.strip())
+    print(f"Initializing '{prog_name}' version {ver}\n")
 
-print("Initiation complete!")
-print()
+    words = []
+    used = []
 
-while True:
-    hint = input("Input hint: ")
-    for word in words:
-        if hint in word and hint not in used:
-            used.append(word)
-            print(f"Unused word containing {hint} = {word}")
-            break
-    print()
+    with open("words_alpha.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            words.append(line.strip())
+
+    print(f"Initiation complete! {len(words)} words loaded into memory.\n\n{'=' * 16}\n")
+
+
+def main() -> None:
+    while True:
+        hint = input("Input hint: ")
+        for word in words:
+            if hint in word and word not in used:
+                used.append(word)
+                print(f"Unused word containing {hint} = {word}")
+                break
+        print(f"\n{'-' * 16}\n")
+
+
+if __name__ == "__main__":
+    setup()
+    main()
