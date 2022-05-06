85ddec6fd541cd1bb4cf5f247e3e4b2ea123ec7f
Melker Widén <melker.widen@gmail.com>
2022-05-06 12:47:55 +0200

    updates

---
+++ Other_Projects/Other/jklm_cheat/README.txt
@@ -0,0 +1,8 @@
+1. Run these commands in your command prompt:
+        pip install selenium
+        pip install webdriver_manager
+        pip install keyboard
+
+1.5. (optional) | Edit settings in config.json.
+
+2. Run "jklm_cheat.py" and enjoy.
--- Other_Projects/Other/jklm_cheat/webscrape_3.py
+++ Other_Projects/Other/jklm_cheat/webscrape_3.py
@@ -7,30 +7,26 @@ import time
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+
 def connect(url) -> None:
     driver.get(url)
     s1 = time.perf_counter()
     btn = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable(("xpath", "/html/body/div[2]/div[3]/form/div[2]/button")))
     e1 = time.perf_counter()
-    # print(f"{btn=} | elapsed time = {e1 - s1}")
+    print(f"{btn=} | elapsed time = {e1 - s1}")
     btn.click()
 
     s2 = time.perf_counter()
-    # iframe = WebDriverWait(driver, 10).until(EC.presence_of_element_located(("xpath", "//iframe[@src='https://falcon.jklm.fun/games/bombparty']")))
     iframe = WebDriverWait(driver, 120).until(EC.presence_of_element_located(("xpath", "//iframe")))
     e2 = time.perf_counter()
     driver.switch_to.frame(iframe)
-    # print(f"{iframe=} | elapsed time = {e2 - s2}")
+    print(f"{iframe=} | elapsed time = {e2 - s2}")
 
 
 def get_syllable() -> str:
     syllable = WebDriverWait(driver, 120).until(EC.visibility_of_element_located(("xpath", "//div[@class='syllable']")))
     return syllable.text
-
-
-def enter_word(word: str) -> None:
-    driver.find_element("xpath", "")
 
 
 if __name__ == "__main__":
