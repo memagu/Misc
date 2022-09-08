4079954cd216a73af6b375ade0d5a5b076fd2c48
Melker Widén <melker.widen@gmail.com>
2022-09-08 21:07:34 +0200

    updates

--- Math/euler method/euler_method_vis.py
+++ Math/euler method/euler_method_vis.py
@@ -9,9 +9,12 @@ display = pygame.display.set_mode(window_resolution, pygame.RESIZABLE)
 pygame.display.set_caption(__file__.split("\\")[-1])
 
 run = True
-fps = 480  # 0 for unlimited
+fps = 480 # 0 for unlimited
 time_prev = time.time()
 clock = pygame.time.Clock()
+
+pygame.font.init()
+font = pygame.font.SysFont("leelawadeeuisemilight", int(window_resolution.y) >> 5)
 
 
 class Color:
@@ -26,11 +29,6 @@ class Color:
     kc_gray = (29, 29, 29)
 
 
-# Text
-pygame.font.init()
-font = pygame.font.SysFont("leelawadeeuisemilight", int(window_resolution.y / 32))
-
-
 def show_fps(delta_time, text_color=(0, 255, 0), outline_color=(0, 0, 0)):
     text = f"FPS: {int(1 / delta_time)}"
     fps_text = font.render(text, True, text_color)
@@ -42,19 +40,52 @@ def show_fps(delta_time, text_color=(0, 255, 0), outline_color=(0, 0, 0)):
     display.blit(fps_text, (0, 0))
 
 
+class Curve:
+    def __init__(self, diff_func, x, y):
+        self.diff_func = diff_func
+        self.x = x
+        self.y = y
+
+    def draw(self, n_iterations, step_size, draw_scale):
+        right_x, left_x = self.x, self.x
+        right_y, left_y = self.y, self.y
+
+        for _ in range(n_iterations):
+            last_right_x = right_x
+            last_right_y = right_y
+            right_y += y_(right_x, right_y) * step_size
+            right_x += step_size
+
+            right_point_start = pygame.math.Vector2(last_right_x, max(min(last_right_y * -1, window_resolution.y),
+                                                          -window_resolution.y)) * draw_scale + origin
+            right_point_end = pygame.math.Vector2(right_x, max(min(right_y * -1, window_resolution.y),
+                                                   -window_resolution.y)) * draw_scale + origin
+
+            last_left_x = left_x
+            last_left_y = left_y
+            left_y += y_(left_x, left_y) * -step_size
+            left_x -= step_size
+
+            left_point_start = pygame.math.Vector2(last_left_x, max(min(last_left_y * -1, window_resolution.y),
+                                                          -window_resolution.y)) * draw_scale + origin
+            left_point_end = pygame.math.Vector2(left_x, max(min(left_y * -1, window_resolution.y),
+                                                   -window_resolution.y)) * draw_scale + origin
+
+            pygame.draw.aaline(display, Color.white, right_point_start, right_point_end)
+            pygame.draw.aaline(display, Color.white, left_point_start, left_point_end)
+
+
 def y_(x, y):
-    return 4 - x**2 * y**3
+    return min(max(4 - x * y, -2 ** 31), 2 ** 31 - 1)
 
 
 n_iterations = 10000
 step_size = 0.01
-
 draw_scale = 1 / step_size
 
-while run:
-    x = 0
-    y = 1
+c1 = Curve(y_, 0, 1)
 
+while run:
     # Calculate dt
     time_now = time.time()
     dt = time_now - time_prev
@@ -72,9 +103,9 @@ while run:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        draw_scale += 10 * dt
+        draw_scale = min(draw_scale + 10 * dt, max(window_resolution) / 2)
     if keys[pygame.K_DOWN]:
-        draw_scale -= 10 * dt
+        draw_scale = max(draw_scale - 10 * dt, 1)
     if keys[pygame.K_w]:
         origin.y -= 50 * dt
     if keys[pygame.K_s]:
@@ -113,18 +144,7 @@ while run:
         draw_y = i * draw_scale + origin.y
         pygame.draw.aaline(display, Color.gray, (0, draw_y), (window_resolution.x, draw_y))
 
-    for i in range(-n_iterations, n_iterations):
-        last_x = x
-        last_y = y
-        y += y_(x, y) * step_size
-        x += step_size
-
-        point_start = pygame.math.Vector2(last_x, max(min(last_y * -1, window_resolution.y),
-                                                      -window_resolution.y)) * draw_scale + origin
-        point_end = pygame.math.Vector2(x, max(min(y * -1, window_resolution.y),
-                                               -window_resolution.y)) * draw_scale + origin
-
-        pygame.draw.aaline(display, Color.white, point_start, point_end)
+    c1.draw(n_iterations, step_size, draw_scale)
 
     show_fps(dt)
     pygame.display.update()
--- Testing/test.py
+++ Testing/test.py
@@ -1,34 +1,5 @@
-import typing import Optional
-# import typing import List
-#
-# def to_int(list1: Optional[ListNode]):
-#     return int(s)
-#
-#
-# def mergeTwoLists(list1: Optional[List], list2: Optional[List]) -> Optional[List]:
-#     #
-#
-# def leetcode():
-#     l1 = [1,3,4]
-#     l2 = [6,3,7]
-#
-#     if mergeTwoLists(l1, l2) == [1,3,3,4,6,7]:
-#         return True
+def t():
+    pass
 
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-ln = ListNode(1, ListNode(2, ListNode(3)))
-print(ln.val)
-print(ln.next.next.val)
-
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        list1 = list1.append(list2)
-        list1.sort()
-        return(list1)
+print(type(t))
 
