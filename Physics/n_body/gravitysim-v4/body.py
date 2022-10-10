34762b4bb3daf1355a26490b8fc9e5f78b26bd7c
Melker Widén <melker.widen@gmail.com>
2022-10-10 13:35:52 +0200

    updates and new projects

---
+++ Physics/n_body/gravitysim-v4/body.py
@@ -0,0 +1,27 @@
+import pygame.draw
+from pygame import Surface
+from pygame.math import Vector3
+from random import randint
+
+
+class Body:
+    def __init__(self, mass: float, radius: float, pos: Vector3, vel: Vector3 = Vector3(), color: Vector3 = None):
+        self.mass = mass
+        self.radius = radius
+        self.pos = pos
+        self.vel = vel
+        self.color = color or Vector3(randint(0, 255), randint(0, 255), randint(0, 255))
+        self.acc = Vector3()
+
+    def update(self, dt: float):
+        self.vel += self.acc * dt
+        self.pos += self.vel * dt
+
+    def draw(self, surface: Surface, scale_distance: float, scale_radius: float, center: Vector3 = Vector3()):
+        print(center + self.pos * scale_distance)
+        pygame.draw.circle(surface, self.color, (center + self.pos * scale_distance).xy, self.radius * scale_radius)
+
+
+
+
+
---
+++ Physics/n_body/gravitysim-v4/main.py
@@ -0,0 +1,60 @@
+import time
+
+import pygame
+from pygame.math import Vector2, Vector3
+
+import body
+
+DRAWSCALE_DISTANCE = 5e-9
+DRAWSCALE_RADIUS = 5e-8
+G = 6.67e-11
+
+pygame.init()
+
+window_resolution = Vector2(1200, 900)
+display = pygame.display.set_mode(window_resolution, pygame.RESIZABLE)
+pygame.display.set_caption(__file__.split("\\")[-1])
+
+framerate = 480
+time_prev = time.perf_counter()
+clock = pygame.time.Clock()
+run = True
+
+sun = body.Body(1.98847e+30, 6.963400e+08, Vector3(), Vector3(), Vector3(255, 255, 0))
+earth = body.Body(5.972e+24, 6.371e+6, Vector3(152.10e+9, 0, 0), Vector3(0, 29.29e+3, 0), Vector3(0, 128, 255))
+moon = body.Body(7.34767309e+22, 1.7381e+06 * 100, Vector3(earth.pos.x + 4.054e+8, 0, 0), Vector3(0, 9.7e+2, 0), Vector3(128, 128, 128))
+
+bodies = [sun, earth, moon]
+
+while run:
+
+    time_now = time.perf_counter()
+    dt = time_now - time_prev
+    time_prev = time_now
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+        if event.type == pygame.VIDEORESIZE:
+            window_resolution.xy = event.w, event.h
+            display = pygame.display.set_mode(window_resolution, pygame.RESIZABLE)
+
+        # if event.type == pygame.KEYDOWN:
+        #     if event.key == pygame.K_1:
+        #         pass
+
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_1]:
+        #     pass
+
+    display.fill(Vector3(0, 0, 0))
+
+    for body in bodies:
+        center = Vector3(*(window_resolution / 2), 0)
+        center = Vector3(0, center.y, 0)
+        body.draw(display, DRAWSCALE_DISTANCE, DRAWSCALE_RADIUS, center)
+
+    pygame.display.update()
+    if abs(framerate):
+        clock.tick(framerate)
