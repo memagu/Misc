0b9039bfd5bbff22fbbc5c7848d8e7ac59f171cf
Melker Widén <melker.widen@gmail.com>
2022-11-08 00:17:19 +0100

    projects added or updated

--- Other_Projects/Other/age_to_color.py
+++ Other_Projects/Other/age_to_color.py
@@ -7,7 +7,7 @@ month = int(dob[4:6])
 day = int(dob[6:])
 
 r = year % 256
-g = month * int(255/12)
-b = day * int(255/31)
+g = month * int(255 / 12)
+b = day * int(255 / 31)
 
 os.system(f"start chrome https://www.google.com/search?q=rgb({r}%2C+{g}%2C+{b})")
