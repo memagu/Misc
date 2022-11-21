a7848fc6c5c6b4a3d93e250a29c7a90732fbf413
Melker Widén <melker.widen@gmail.com>
2022-11-21 13:44:13 +0100

    updates and new projects

---
+++ Other_Projects/Other/pgn_parser.py
@@ -0,0 +1,44 @@
+data_raw = []
+while True:
+    raw_input = input()
+    if not raw_input:
+        break
+
+    data_raw += raw_input.split()
+
+result = data_raw.pop()
+
+data = []
+for i in range(0, len(data_raw) - 1, 3):
+    white_move = data_raw[i + 1]
+    black_move = '' if i + 2 >= len(data_raw) else data_raw[i + 2]
+    data.append((white_move, black_move))
+
+moves = {"pawn": ["pawn", 0],
+         "B": ["bishop", 0],
+         "N": ["knight", 0],
+         "R": ["rook", 0],
+         "Q": ["queen", 0],
+         "K": ["king", 0],
+         "0-0-0": ["queen side castle", 0],
+         "0-0": ["king side castle", 0]}
+
+for white_move, black_move in data:
+    if white_move == "0-0-0":
+        moves["0-0-0"][1] += 1
+        continue
+
+    if white_move == "0-0":
+        moves["0-0"][1] += 1
+        continue
+
+    if (piece := white_move[0]) in moves:
+        moves[piece][1] += 1
+        continue
+
+    moves["pawn"][1] += 1
+
+print(f"move{:>20}move_count")
+for move, move_count in moves.values():
+    pass
+print([(move, move_count) for move, move_count in moves.values()])
