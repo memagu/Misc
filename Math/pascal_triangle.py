0e4f0f7a8531fe3be4bd7a5cc9170105a2b0a072
Melker Widén <melker.widen@gmail.com>
2023-03-23 23:40:27 +0100

    projects added or updated

---
+++ Math/pascal_triangle.py
@@ -0,0 +1,21 @@
+def pascal_triangle(n: int) -> list[int]:
+    if n < 0:
+        raise Exception("n must be an integer larger than one")
+
+    yield [1]
+
+    row = [1]
+
+    for _ in range(n):
+        temp = [1]
+        for i in range(len(row)-1):
+            temp.append(row[i] + row[i+1])
+        temp.append(1)
+        row = temp
+
+        yield row
+
+
+if __name__ == "__main__":
+    for row in pascal_triangle(24):
+        print(" ".join(map(lambda x: str(x).center(8), row)).center(256))
