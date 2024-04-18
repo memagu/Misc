64818ec16c25542a847b91c5abce0ba5d06ea25b
Melker Widén <melker.widen@gmail.com>
2024-04-19 00:16:23 +0200

    Call monitor_file with POLLING_INTERVAL_SECONDS, MAX_READ_SIZE and change the order of monitor_file's arguments

--- tools/live_log.py
+++ tools/live_log.py
@@ -7,16 +7,16 @@ POLLING_INTERVAL_SECONDS = 0.1
 MAX_READ_SIZE = 1 << 17  # 128 KiB
 
 
-def monitor_file(file: Path, max_read_size: int = -1, polling_interval_seconds: float = 0.1) -> Never:
+def monitor_file(file: Path, polling_interval_seconds: float = 0.1, max_read_size: int = -1) -> Never:
     """
     Continuously monitors a file for new entries and prints them out as they are added.
 
     :param file: Path to the file to be monitored.
     :type file: pathlib.Path
-    :param max_read_size: Maximum number of bytes to read at a time. If set to -1, the function will read as much data as available without limit.
-    :type max_read_size: int
     :param polling_interval_seconds: Interval in seconds at which to attempt reading new data from the file.
     :type polling_interval_seconds: float
+    :param max_read_size: Maximum number of bytes to read at a time. If set to -1, the function will read as much data as available without limit.
+    :type max_read_size: int
     :return: This function never returns.
     :rtype: typing.Never
     """
@@ -44,7 +44,7 @@ def main() -> None:
     parser.add_argument("file", type=Path, help="The file to be monitored.")
     args = parser.parse_args()
 
-    monitor_file(args.file)
+    monitor_file(args.file, POLLING_INTERVAL_SECONDS, MAX_READ_SIZE)
 
 
 if __name__ == "__main__":
